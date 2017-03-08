f588e57689b8f14238b7d93da503035bd012f059
ariya piriya <ariyapiriyawiwatwong@gmail.com>
2017-03-08 04:32:11 -0600

    show graph ok haha eiei

--- liveRandomData.php
+++ liveRandomData.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <html>
-hiiii
+
 <head>
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<script type="text/javascript">
@@ -10,7 +10,7 @@ hiiii
 
 		var chart = new CanvasJS.Chart("chartContainer",{
 			title :{
-				text: "Live Random Data Jaaa"
+				text: "Live Random Data"
 			},
 			data: [{
 				type: "line",
@@ -20,35 +20,39 @@ hiiii
 
 		var xVal = 0;
 		var yVal = 100;
-		var updateInterval = 100;
-		var dataLength = 500; // number of dataPoints visible at any point
+		var updateInterval =5000;
+		var dataLength = 10; // number of dataPoints visible at any point
 
 		var updateChart = function (count) {
 			count = count || 1;
 			// count is number of times loop runs to generate random dataPoints.
-
-			for (var j = 0; j < count; j++) {
-				yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-				dps.push({
-					x: xVal,
-					y: yVal
-				});
-				xVal++;
-			};
-			if (dps.length > dataLength)
-			{
-				dps.shift();
-			}
-
-			chart.render();
-
+      $.ajax({
+        url: '/testGetDataToGraph.php',
+        method: 'GET'
+      }).done(function(response) {
+        for (var j = 0; j < count; j++) {
+          yVal = parseInt(response);
+          console.log(yVal);
+          console.log(xVal);
+          dps.push({
+            x: new Date(),
+            y: yVal
+          });
+          xVal++;
+        };
+        if (dps.length > dataLength)
+        {
+          dps.shift();
+        }
+        chart.render();
+        updateChart(dataLength);
+      });
 		};
 
 		// generates first set of dataPoints
 		updateChart(dataLength);
 
 		// update chart after specified time.
-		setInterval(function(){updateChart()}, updateInterval);
 
 	}
 	</script>
